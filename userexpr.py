7130616c04e116359abcdcfe7cee93a5b8da555d
Andrew Hicks <ahick17@lsu.edu>
2022-01-07 16:51:40 -0600

    Reworked yaml load in userexpr.py, dump not impl.

--- userexpr.py
+++ userexpr.py
@@ -4,56 +4,63 @@ from sympyplus import *
 import yaml
 from loaddump import *
 
-class FromTensor(Matrix):
-    def __new__(cls,tensor):
-        M = Matrix(tensor).reshape(3,3)
+class FromSympy(Matrix):
+    def __new__(cls,matrix:list):
+        return super().__new__(cls,matrix)
+    @property
+    def result(self):
+        return None
+    def uflfy(self):
+        return uflfy(self.result)
+def fs_constructor(cls):
+    def constructor(loader,node):
+        value = loader.construct_sequence(node)
+        return cls(value)
+    return constructor
+def fs_representer(tag):
+    def representer(dumper,data):
+        raise NotImplementedError('YAML dump not implemented for user expressions.')
+        # data = map(str,data)
+        # data = ','.join(data)
+        # data = '[' + data + ']'
+        # return dumper.represent_scalar(tag,data)
+    return representer
+
+class FromTensor(FromSympy):
+    @property
+    def result(self):
+        M = self.reshape(3,3)
         M = SymmetricTracelessMatrix(M)
         m = vectorfy(M)
-        return super().__new__(cls,m)
-    def __init__(self,tensor):
-        self.tensor = Matrix(tensor).reshape(3,3)
+        return m
     def __repr__(self):
-        return f'FromTensor {self.tensor}'
-    def uflfy(self):
-        return uflfy(self)
+        return f'FromTensor {self.result}'
+yaml.add_constructor('!FromTensor',fs_constructor(FromTensor))
+yaml.add_representer(FromTensor,fs_representer('!FromTensor'))
 
-def ft_constructor(loader,node):
-    value = loader.construct_sequence(node)
-    return FromTensor(value)
-
-yaml.add_constructor('!FromTensor',ft_constructor)
-
-class FromVector(Matrix):
-    def __new__(cls,vector):
-        return super().__new__(cls,vector)
+class FromVector(FromSympy):
+    def __new__(cls,matrix:list):
+        if len(matrix) != 5:
+            raise ValueError('FromVector must be length 5.')
+        return super().__new__(cls,matrix)
+    @property
+    def result(self):
+        return self
     def __repr__(self):
-        return f'FromVector {self}'
-    def uflfy(self):
-        return uflfy(self)
+        return f'FromVector {self.result}'
+yaml.add_constructor('!FromVector',fs_constructor(FromVector))
+yaml.add_representer(FromVector,fs_representer('!FromVector'))
 
-def fv_constructor(loader,node):
-    value = loader.construct_sequence(node)
-    return FromVector(value)
-
-yaml.add_constructor('!FromVector',fv_constructor)
-
-class FromDirector(Matrix):
-    def __new__(cls,director):
+class FromDirector(FromSympy):
+    @property
+    def result(self):
         from config import constants as c
-        n = Matrix(director)
+        n = Matrix(self)
         n = n/sqrt(n[0]**2+n[1]**2+n[2]**2)
         M = c.S0*(outerp(n,n) - 1/3*eye(3))
         m = vectorfy(M)
-        return super().__new__(cls,m)
-    def __init__(self,director):
-        self.director = Matrix(director)
+        return m
     def __repr__(self):
-        return f'FromDirector {self.director}'
-    def uflfy(self):
-        return uflfy(self)
-
-def fd_constructor(loader,node):
-    value = loader.construct_sequence(node)
-    return FromDirector(value)
-
-yaml.add_constructor('!FromDirector',fd_constructor)
+        return f'FromDirector {self.result}'
+yaml.add_constructor('!FromDirector',fs_constructor(FromDirector))
+yaml.add_representer(FromDirector,fs_representer('!FromDirector'))
