f95c7ea72198a326b50436511d0c61be4506558f
Andrew Hicks <ahick17@lsu.edu>
2022-02-24 15:57:28 -0600

    Initial energy now prints for resume and overwrite

--- firedrakeplus.py
+++ firedrakeplus.py
@@ -300,7 +300,7 @@ def solve_PDE(mesh,refinement_level='Not specified'):
         # On resume mode, q_soln and q_prev are loaded from a previous state
         q_soln = saves.load_checkpoint(H1_vec,'q_soln')
         q_prev = saves.load_checkpoint(H1_vec,'q_prev')
-        pr.Print(f'Initial energy = {compute_energy(q_soln)}')
+        pr.info(f'E={compute_energy(q_soln):.5f} @INITIAL')
         times, energies = saves.load_energies()
         if len(times) != len(energies):
             raise ValueError(f'Number of times {len(times)} and number of energies {len(energies)} not equal.')
@@ -308,7 +308,7 @@ def solve_PDE(mesh,refinement_level='Not specified'):
     else:
         # On overwrite mode or when save is turned off, q_soln and q_prev are taken to be the initial guess
         q_soln = interpolate(eval(initial_q),H1_vec)
-        # print(compute_energy(q_soln))
+        pr.info(f'E={compute_energy(q_soln):.5f} @INITIAL')
         q_prev.assign(q_soln)
         times, energies = saves.TimeList([]), saves.EnergyList([])
         t_init = 0
