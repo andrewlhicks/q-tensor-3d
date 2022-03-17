c7ae21c1149c5c7f7925e84bc2a8d91e79ae1b27
Andrew Hicks <ahick17@lsu.edu>
2022-03-17 16:47:03 -0500

    Cleaned up compute.py

--- compute.py
+++ compute.py
@@ -1,12 +1,10 @@
 from sympyplus import *
 
-import user_expressions.initial_q as initial_q
 import user_expressions.manufac_q as manufac_q
 import user_expressions.forcing_f as forcing_f
 import user_expressions.forcing_g as forcing_g
 import user_expressions.bdycond_s as bdycond_s
 
-from config import settings
 from config import constants as c
 
 # Set up Qvector objects
@@ -25,6 +23,7 @@ P = p.tens
 
 r = QVector('r')
 Dr = r.grad
+Drr = Param([Dr,r])
 R = r.tens
 
 qp = QVector('q_prev')
@@ -47,10 +46,6 @@ c.S0 = 0.700005530940965
 
 Q0 = c.S0*(outerp(nu,nu) - (1.0/3.0)*eye(3))
 Pi = eye(3) - outerp(nu,nu)
-
-# theta = atan2(x[1]-0.5,x[0]-0.5)
-# N = Matrix([cos(theta),sin(theta),0])
-# Q0 = c.S0*(outerp(N,N) - (1/3)*eye(3))
 
 #################################################################
 
@@ -75,54 +70,47 @@ def termL3(grad1,grad2):
 
     return term
 
-# def term_twist(q):
-#     """ Returns the twist term from the energy """
-#     Q = QTensor(q)
-
-#     return c.q0*mixedp(Q,Q) + c.q0**2*innerp(Q,Q)
-
 def term_twist_var(q,p):
     """ Returns the variational derivative of the twist term """
     Q = QTensor(q)
     P = QTensor(p)
 
-    # return 2*c.q0*mixedp(Q,P) + 2*c.q0*mixedp(P,Q) + 4*c.q0**2*innerp(Q,P)
-    return 2*c.q0*mixedp(Q,P) + 2*c.q0*mixedp(P,Q) # got rid of the linear 0-th derivative term
+    return 2*c.q0*mixedp(Q,P) + 2*c.q0*mixedp(P,Q)
 
 #########################################################
 
 def compute():
     # Define 'energies' used to calculate the energy    
-    energies = EnergyForm([Dq,q],[Dp,p],[Dr,r])
-    energies.add_domain(GeneralForm(c.L1/2*termL1(Dq,Dq)+c.L2/2*termL2(Dq,Dq)+c.L3/2*termL3(Dq,Dq),[Dq,q],name='Elastic Energy'))
-    energies.add_domain(GeneralForm(2*c.L1*c.q0*mixedp(Q,Q),[Dq,q],name='Twist Energy'))
-    energies.add_domain(GeneralForm((1/c.ep**2)*(1 - (c.A/2)*innerp(Q,Q) - (c.B/3)*trace(Q**3) + (c.C/4)*trace(Q**2)**2),[Dq,q],name='Bulk Energy'))
-    energies.add_domain(GeneralForm(-f.dot(q),[Dq,q],name='Domain Forcing Energy'))
-    energies.add_boundary(GeneralForm(c.W0/2*innerp(Q-Q0,Q-Q0),[Dq,q],name='Homeotropic Anchoring'))
-    energies.add_boundary(GeneralForm(c.W1/2*innerp(Q-Pi*Q*Pi+c.S0/3*outerp(nu,nu),Q-Pi*Q*Pi+c.S0/3*outerp(nu,nu))+c.W2/4*(innerp(Q,Q)-2*c.S0**2/3)**2,[Dq,q],name='Planar-degenerate Anchoring'))
-    energies.add_boundary(GeneralForm(-g.dot(q),[Dq,q],name='Boundary Forcing Energy'))
+    energies = EnergyForm(Dqq,Dpp,Drr)
+    energies.add_domain(GeneralForm(c.L1/2*termL1(Dq,Dq)+c.L2/2*termL2(Dq,Dq)+c.L3/2*termL3(Dq,Dq),Dqq,name='Elastic Energy'))
+    energies.add_domain(GeneralForm(2*c.L1*c.q0*mixedp(Q,Q),Dqq,name='Twist Energy'))
+    energies.add_domain(GeneralForm((1/c.ep**2)*(1 - (c.A/2)*innerp(Q,Q) - (c.B/3)*trace(Q**3) + (c.C/4)*trace(Q**2)**2),Dqq,name='Bulk Energy'))
+    energies.add_domain(GeneralForm(-f.dot(q),Dqq,name='Domain Forcing Energy'))
+    energies.add_boundary(GeneralForm(c.W0/2*innerp(Q-Q0,Q-Q0),Dqq,name='Homeotropic Anchoring'))
+    energies.add_boundary(GeneralForm(c.W1/2*innerp(Q-Pi*Q*Pi+c.S0/3*outerp(nu,nu),Q-Pi*Q*Pi+c.S0/3*outerp(nu,nu))+c.W2/4*(innerp(Q,Q)-2*c.S0**2/3)**2,Dqq,name='Planar-degenerate Anchoring'))
+    energies.add_boundary(GeneralForm(-g.dot(q),Dqq,name='Boundary Forcing Energy'))
 
     # Construct PDE
 
     lhs_d = [ # lhs domain
-        GeneralForm((1/c.dt)*q.dot(p),[Dq,q],[Dp,p],name='Q:P/dt'),
-        GeneralForm(c.L1*termL1(Dq,Dp)+c.L2*termL2(Dq,Dp)+c.L3*termL3(Dq,Dp),[Dq,q],[Dp,p],name='a_E(Q,P)'),
-        GeneralForm(c.L1*term_twist_var(q,p),[Dq,q],[Dp,p],name='a_T(Q,P)'),
-        GeneralForm((1/c.ep**2)*(-c.A*innerp(Q,P) - c.B*innerp(Q**2,P) + c.C*innerp(Q,Q)*innerp(Q,P)),[Dq,q],[Dp,p],name='Dψ(Q):P')
+        GeneralForm((1/c.dt)*q.dot(p),Dqq,Dpp,name='Q:P/dt'),
+        GeneralForm(c.L1*termL1(Dq,Dp)+c.L2*termL2(Dq,Dp)+c.L3*termL3(Dq,Dp),Dqq,Dpp,name='a_E(Q,P)'),
+        GeneralForm(c.L1*term_twist_var(q,p),Dqq,Dpp,name='a_T(Q,P)'),
+        GeneralForm((1/c.ep**2)*(-c.A*innerp(Q,P) - c.B*innerp(Q**2,P) + c.C*innerp(Q,Q)*innerp(Q,P)),Dqq,Dpp,name='Dψ(Q):P')
         ]
     rhs_d = [ # rhs domain
-        GeneralForm((1/c.dt)*qp.dot(p),[Dp,p],name='Q_p:P/dt'),
-        GeneralForm(f.dot(p),[Dp,p],name='f(P)')
+        GeneralForm((1/c.dt)*qp.dot(p),Dpp,name='Q_p:P/dt'),
+        GeneralForm(f.dot(p),Dpp,name='f(P)')
     ]
     lhs_b = [ # lhs boundary
-        GeneralForm(c.W0*q.dot(p),[Dq,q],[Dp,p],name='W0(Q:P)'),
-        GeneralForm(c.W1*innerp(Q-Pi*Q*Pi,P),[Dq,q],[Dp,p],name='W1(Q-ΠQΠ):P'),
-        GeneralForm(c.W2*((innerp(Q,Q) - 2*c.S0**2/3)*innerp(Q,P)),[Dq,q],[Dp,p],name='(|Q|^2-2S0^2/3)(Q:P)')
+        GeneralForm(c.W0*q.dot(p),Dqq,Dpp,name='W0(Q:P)'),
+        GeneralForm(c.W1*innerp(Q-Pi*Q*Pi,P),Dqq,Dpp,name='W1(Q-ΠQΠ):P'),
+        GeneralForm(c.W2*((innerp(Q,Q) - 2*c.S0**2/3)*innerp(Q,P)),Dqq,Dpp,name='(|Q|^2-2S0^2/3)(Q:P)')
     ]
     rhs_b = [ # rhs boundary
-        GeneralForm(c.W0*innerp(Q0,P),[Dp,p],name='W0(Q0:P)'),
-        GeneralForm(c.W1*innerp(-c.S0/3*outerp(nu,nu),P),[Dp,p],name='(-S0/3(nu⊗nu):P)'),
-        GeneralForm(g.dot(p),[Dp,p],name='g(P)')
+        GeneralForm(c.W0*innerp(Q0,P),Dpp,name='W0(Q0:P)'),
+        GeneralForm(c.W1*innerp(-c.S0/3*outerp(nu,nu),P),Dpp,name='(-S0/3(nu⊗nu):P)'),
+        GeneralForm(g.dot(p),Dpp,name='g(P)')
     ]
 
     # assemble two PDEs
@@ -136,7 +124,6 @@ def compute():
     # Create relevant UFL strings
 
     out = {
-        'initial_q' : initial_q.out,
         'manufac_q' : manufac_q.out,
         'forcing_f' : forcing_f.out,
         'forcing_g' : forcing_g.out,
