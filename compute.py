90c5397aa4e21b53c5c7a2c39c63dbfb2e42a52a
Andrew Hicks <ahick17@lsu.edu>
2022-11-10 14:13:54 -0600

    Readded L1 constant to twist term

--- compute.py
+++ compute.py
@@ -81,7 +81,7 @@ def compute():
     # Define 'energies' used to calculate the energy    
     energies = EnergyForm(Dqq,Dpp,Drr)
     energies.add_domain(GeneralForm(c.L1/2*termL1(Dq,Dq)+c.L2/2*termL2(Dq,Dq)+c.L3/2*termL3(Dq,Dq),Dqq,name='Elastic Energy'))
-    energies.add_domain(GeneralForm(2*c.q0*mixedp(Q,Q),Dqq,name='Twist Energy'))
+    energies.add_domain(GeneralForm(2*c.q0*c.L1*mixedp(Q,Q),Dqq,name='Twist Energy'))
     energies.add_domain(GeneralForm((1/c.ep**2)*(1 - (c.A/2)*innerp(Q,Q) - (c.B/3)*trace(Q**3) + (c.C/4)*trace(Q**2)**2),Dqq,name='Bulk Energy'))
     energies.add_domain(GeneralForm(-f.dot(q),Dqq,name='Domain Forcing Energy'))
     energies.add_boundary(GeneralForm(c.W0/2*innerp(Q-Q0,Q-Q0),Dqq,name='Homeotropic Anchoring'))
@@ -95,7 +95,7 @@ def compute():
 
     lhs_d = [ # lhs domain
         GeneralForm(c.L1*termL1(Dq,Dp)+c.L2*termL2(Dq,Dp)+c.L3*termL3(Dq,Dp),Dqq,Dpp,name='a_E(Q,P)'),
-        GeneralForm(2*c.q0*mixedp(Q,P) + 2*c.q0*mixedp(P,Q),Dqq,Dpp,name='a_T(Q,P)'),
+        GeneralForm(2*c.q0*c.L1*(mixedp(Q,P) + mixedp(P,Q)),Dqq,Dpp,name='a_T(Q,P)'),
         GeneralForm((1/c.ep**2)*(-c.A*innerp(Q,P) - c.B*innerp(Q**2,P) + c.C*innerp(Q,Q)*innerp(Q,P)),Dqq,Dpp,name='Dψ(Q):P')
         ]
     rhs_d = [ # rhs domain
