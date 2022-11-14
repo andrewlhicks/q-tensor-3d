5a2b3c396b0530829a21831fca3cfd85e635d396
Andrew Hicks <ahick17@lsu.edu>
2022-11-14 17:33:58 -0600

    Fully added rhs f/g and manu_q into EqnGlobals

--- firedrakeplus/eqnglobals.py
+++ firedrakeplus/eqnglobals.py
@@ -11,9 +11,10 @@ def set_eqn_globals(comp,uflcache_dict):
         pde_pd = comp['pde_pd']
 
         initial_q = uflcache_dict['initcond'] # uflcache will be preferred over comp in the future
+        manu_q = uflcache_dict['manu_q'] if 'manu_q' in uflcache_dict.keys() else 'as_vector([0,0,0,0,0])'
 
-        forcing_f = comp['forcing_f'] if settings.options.manufactured else 'as_vector([0,0,0,0,0])' # NOTE: while it works to calculate f here as an interpolation with Firedrake, it's actually more precise to do it in sympy beforehand.
-        forcing_g = comp['forcing_g'] if settings.options.manufactured else 'as_vector([0,0,0,0,0])'
+        forcing_f = uflcache_dict['forcing_f'] if 'forcing_f' in uflcache_dict.keys() else 'as_vector([0,0,0,0,0])' # NOTE: while it works to calculate f here as an interpolation with Firedrake, it's actually more precise to do it in sympy beforehand.
+        forcing_g = uflcache_dict['forcing_g'] if 'forcing_g' in uflcache_dict.keys() else 'as_vector([0,0,0,0,0])'
         s_bdy = uflcache_dict['s_bdy'] if 's_bdy' in uflcache_dict.keys() else 'as_vector([0,0,0,0,0])' # warning: may want to consider this carefully
 
         w_bdy_nu = uflcache_dict['w_bdy_nu'] if 'w_bdy_nu' in uflcache_dict.keys() else 'FacetNormal(mesh)'
