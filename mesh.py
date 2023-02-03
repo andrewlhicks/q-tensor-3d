0a0465f116b29909a885901d8f5df13dcd2a7bdc
Andrew Hicks <ahick17@lsu.edu>
2023-02-03 16:09:34 -0600

    Updated mesh.py script

--- mesh.py
+++ mesh.py
@@ -1,28 +1,32 @@
-import os
 import getopt
 import sys
 import mymesh
 import numpy as np
 
-def main():
-    mesh_path = sys.argv[1]
-    mesh_type = sys.argv[2]
+def usage():
+    usage_str = """usage: python mesh.py (-c <center>) (-r <radius) (-s <mesh_size>) <mesh_type> <mesh_path>
+  c: specifies center <center> of inner sphere, should be comma-separated with no spaces
+  r: specifies radius <radius> of inner sphere
+  s: specifies mesh size <mesh_size> of mesh
+"""
+    print(usage_str)
 
-    if mesh_type not in ('shell'):
-        raise NotImplementedError('only shell mesh implemented')
-    
+def main():
     try:
-        opts, listargs = getopt.getopt(sys.argv[3:],'c:r:s:',[])
+        opts, listargs = getopt.getopt(sys.argv[1:],'c:r:s:',['help'])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)
         print('use --help for usage')
         sys.exit()
 
-    kwargs = {'path':mesh_path}
+    kwargs = {}
 
     for o, a in opts:
-        if o in ('-c'):
+        if o in ('--help'):
+            usage()
+            sys.exit()
+        elif o in ('-c'):
             inner_center = np.array(a.split(',')).astype(np.float64)
             kwargs.update({'inner_center':inner_center})
         elif o in ('-r'):
@@ -31,7 +35,20 @@ def main():
             kwargs.update({'mesh_size':float(a)})
         else:
             sys.exit()
-    print(kwargs)
+    
+    if (lenargs := len(listargs)) != 2:
+        print(f'exactly 2 list args required, not {lenargs}')
+        sys.exit()
+
+    mesh_type = listargs[0]
+    mesh_path = listargs[1]
+
+    kwargs.update({'path':mesh_path})
+
+    if mesh_type not in ('shell'):
+        print('only shell mesh implemented')
+        sys.exit()
+    
     mymesh.CreateShellMesh(**kwargs)
 
 if __name__ == '__main__':
