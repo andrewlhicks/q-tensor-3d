c07740bc34c65aff7a35f19b321bd5f130fe2f14
Andrew Hicks <ahick17@lsu.edu>
2024-07-01 15:35:48 -0500

    Moved some stuff back to ufl_objects.py

--- q3d/uflcache/userexpr/ufl_objects.py
+++ q3d/uflcache/userexpr/ufl_objects.py
@@ -3,6 +3,67 @@ import yaml
 
 __all__ = ('add_ufl_constructors')
 
+domain = AbstractDomain(3,3) # acts as placeholder domain
+x0, x1, x2 = SpatialCoordinate(domain)
+
+def from_director(director: list | ListTensor | Zero) -> ListTensor | Zero:
+    """ vector3d -> qvector """
+    return vectorfy(qtensor_from_director(as_vector(director)))
+
+def from_spherical_director(director: list | ListTensor | Zero) -> ListTensor:
+    """ vector3d -> qvector """
+    return vectorfy(qtensor_from_director(spherical_to_cartesian(as_vector(director))))
+
+def spherical_to_cartesian(vector_spherical: ListTensor | Zero) -> ListTensor | Zero:
+    """ vector3d -> vector3d """
+    from ufl import Constant, replace
+
+    if vector_spherical.ufl_shape != (3,):
+        raise ValueError(f'Spherical vector must have shape (3,), not {vector_spherical.ufl_shape}')
+
+    r = Constant(domain)
+    theta = Constant(domain)
+    phi = Constant(domain)
+
+    r_theta_phi = vector_spherical # get r, theta, and phi coord of initial condition
+    # r_theta_phi = replace(r_theta_phi, {r: sqrt(x0**2+x1**2+x2**2)})
+    # r_theta_phi = replace(r_theta_phi, {theta: acos(x2/sqrt(x0**2+x1**2+x2**2))})
+    # r_theta_phi = replace(r_theta_phi, {phi: sign(x1)*acos(x0/sqrt(x0**2+x1**2))})
+
+    # spherical basis vectors
+    e_r = as_vector([x0, x1, x2])/sqrt(x0**2+x1**2+x2**2)
+    e_theta = as_vector([x0*x2, x1*x2, -(x0**2+x1**2)])/sqrt((x0**2+x1**2+x2**2)*(x0**2+x1**2))
+    e_phi = as_vector([-x1, x0, 0])/sqrt(x0**2+x1**2)
+
+    # remove anything undefined
+    e_r = conditional(ne(x0**2+x1**2+x2**2,0), e_r, as_vector([0,0,0]))
+    e_theta = conditional(ne(x0**2+x1**2,0), e_theta, as_vector([0,0,0]))
+    e_phi = conditional(ne(x0**2+x1**2,0), e_phi, as_vector([0,0,0]))
+
+    # define n as a linear combination of the three basis vectors and scalars
+    vector_cartesian = r_theta_phi[0]*e_r + r_theta_phi[1]*e_theta + r_theta_phi[2]*e_phi
+
+    return vector_cartesian
+
+def qtensor_from_director(director: ListTensor) -> ListTensor:
+    """ vector3d -> qtensor """
+    try:
+        from q3d.config import constants
+        S0: float = constants.S0
+    except ImportError:
+        S0 = 1
+    
+    n: ListTensor = director # get director
+
+    # if the director is 0, then let the Q-tensor be 0
+    if inner(n,n) != 0:
+        M: ListTensor = S0*(outer(n,n)/inner(n,n) - 1/3*Identity(3))
+        M = conditional(ne(inner(n,n),0), M, Zero((3,3)))
+    else:
+        M: Zero = Zero((3,3))
+    
+    return M
+
 def add_ufl_constructors():
     def qvector_constructor(loader, node):
         def qvector(vector5d: ListTensor | Zero) -> ListTensor | Zero:
--- q3d/uflplus/__init__.py
+++ q3d/uflplus/__init__.py
@@ -1,70 +1,9 @@
-from ufl.operators import *
-from ufl.classes import *
-from ufl import Identity, AbstractDomain
-from firedrake.ufl_expr import *
+from ufl import *
+from ufl.classes import * # allows for evaluating UFL cache strings into UFL objects
+from firedrake.ufl_expr import * # overwrites some of ufl.classes using Firedrake-specific UFL object defintinions. Needed for Firedrake solvers to work properly
+from ufl.operators import ListTensor, Zero # allows for type hinting/checking for UFL objects
 
-domain = AbstractDomain(3,3)
-x0, x1, x2 = SpatialCoordinate(domain)
-
-def from_director(director: list | ListTensor | Zero) -> ListTensor | Zero:
-    """ vector3d -> qvector """
-    return vectorfy(qtensor_from_director(as_vector(director)))
-
-def from_spherical_director(director: list | ListTensor | Zero) -> ListTensor:
-    """ vector3d -> qvector """
-    return vectorfy(qtensor_from_director(spherical_to_cartesian(as_vector(director))))
-
-def spherical_to_cartesian(vector_spherical: ListTensor | Zero) -> ListTensor | Zero:
-    """ vector3d -> vector3d """
-    from ufl import Constant, replace
-
-    if vector_spherical.ufl_shape != (3,):
-        raise ValueError(f'Spherical vector must have shape (3,), not {vector_spherical.ufl_shape}')
-
-    r = Constant(domain)
-    theta = Constant(domain)
-    phi = Constant(domain)
-
-    r_theta_phi = vector_spherical # get r, theta, and phi coord of initial condition
-    # r_theta_phi = replace(r_theta_phi, {r: sqrt(x0**2+x1**2+x2**2)})
-    # r_theta_phi = replace(r_theta_phi, {theta: acos(x2/sqrt(x0**2+x1**2+x2**2))})
-    # r_theta_phi = replace(r_theta_phi, {phi: sign(x1)*acos(x0/sqrt(x0**2+x1**2))})
-
-    # spherical basis vectors
-    e_r = as_vector([x0, x1, x2])/sqrt(x0**2+x1**2+x2**2)
-    e_theta = as_vector([x0*x2, x1*x2, -(x0**2+x1**2)])/sqrt((x0**2+x1**2+x2**2)*(x0**2+x1**2))
-    e_phi = as_vector([-x1, x0, 0])/sqrt(x0**2+x1**2)
-
-    # remove anything undefined
-    e_r = conditional(ne(x0**2+x1**2+x2**2,0), e_r, as_vector([0,0,0]))
-    e_theta = conditional(ne(x0**2+x1**2,0), e_theta, as_vector([0,0,0]))
-    e_phi = conditional(ne(x0**2+x1**2,0), e_phi, as_vector([0,0,0]))
-
-    # define n as a linear combination of the three basis vectors and scalars
-    vector_cartesian = r_theta_phi[0]*e_r + r_theta_phi[1]*e_theta + r_theta_phi[2]*e_phi
-
-    return vector_cartesian
-
-def qtensor_from_director(director: ListTensor) -> ListTensor:
-    """ vector3d -> qtensor """
-    try:
-        from q3d.config import constants
-        S0: float = constants.S0
-    except ImportError:
-        S0 = 1
-    
-    n: ListTensor = director # get director
-
-    # if the director is 0, then let the Q-tensor be 0
-    if inner(n,n) != 0:
-        M: ListTensor = S0*(outer(n,n)/inner(n,n) - 1/3*Identity(3))
-        M = conditional(ne(inner(n,n),0), M, Zero((3,3)))
-    else:
-        M: Zero = Zero((3,3))
-    
-    return M
-
-def vectorfy(tensor: ListTensor) -> ListTensor:
+def vectorfy(tensor: ListTensor | Zero) -> ListTensor | Zero:
     """ qtensor -> qvector """
     from math import sqrt
 
