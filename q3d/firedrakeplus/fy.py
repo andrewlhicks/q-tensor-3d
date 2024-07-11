6052f4ed723721c73878ba418c77109be632fa05
Andrew Hicks <ahick17@lsu.edu>
2024-07-11 15:08:23 -0500

    Last touches. The beginning of a new idea?

--- q3d/firedrakeplus/fy.py
+++ q3d/firedrakeplus/fy.py
@@ -1,24 +1,26 @@
 from firedrake import SpatialCoordinate, VectorFunctionSpace, Function
 from q3d.uflplus import *
 
-def firedrakefy(func, mesh): # REMOVE?
-    H1_vec = VectorFunctionSpace(mesh, 'CG', 1, 5)
-    x0, x1, x2 = SpatialCoordinate(mesh)
+def EqnGlobalFunction(func_str, func_space_data):
+    from q3d.firedrakeplus.eqnglobals import EqnGlobals
+
+    H1_vec = VectorFunctionSpace(*func_space_data, 5)
+    x0, x1, x2 = SpatialCoordinate(func_space_data[0])
+
+    func = getattr(EqnGlobals, func_str)
 
     f = Function(H1_vec)
     f.interpolate(eval(func))
 
     return f
 
-def RandomFunction(function_space): # CHANGE to mesh instead of func space?
-    f = Function(function_space)
-    f.interpolate(as_vector([0,0,0,0,0]))
+def RandomFunction(dim, func_space_data): # CHANGE to mesh instead of func space?
+    H1_vec = VectorFunctionSpace(*func_space_data, dim)
+
+    f = Function(H1_vec)
+    f.assign(Zero((dim,)))
 
     for ii in range(len(f.dat.data)):
         f.dat.data[ii] = f.rand(5)*1e+1
 
     return f
-
-def ManuQ(mesh): # REMOVE?
-    from q3d.firedrakeplus.eqnglobals import EqnGlobals
-    return firedrakefy(EqnGlobals.manu_q, mesh)
